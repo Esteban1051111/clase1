7248b9b6399f6f81e3eba8a603a160875ad03930
Esteban <juan.3801911657@ucaldas.edu.co>
2023-09-19 20:50:33 -0500

    primerCommit

---
+++ demo/src/test/java/Proyecto1/demo/UcaldasApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcaldasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
